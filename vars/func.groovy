e129949ee8741d1c576d42e4841bc389bd37165a
Husan Kateryna <dgivasss@gmail.com>
2019-06-17 00:44:58 +0300

    Testing

--- vars/func.groovy
+++ vars/func.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(body) {
-    this.echo "Triggering ${name} stage..."
+    this.echo "Triggering stage..."
     this.stage(name) {
-        script.echo "Triggering in ${name} stage..."
+        script.echo "Triggering in stage..."
     }
     return this
 }
