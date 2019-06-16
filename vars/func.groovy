441056b248a81a8f91979bac3950162e99cc8a6f
Husan Kateryna <dgivasss@gmail.com>
2019-06-17 00:56:48 +0300

    Testing

--- vars/func.groovy
+++ vars/func.groovy
@@ -3,7 +3,7 @@
 def call(body) {
     this.echo "Triggering stage..."
     this.stage("Stage name") {
-        script.echo "Triggering in stage..."
+        this.echo "Triggering in stage..."
     }
     return this
 }
