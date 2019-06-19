1fabefc5bc5dd97910e3ae3b60290d6e7df0250f
Husan Kateryna <dgivasss@gmail.com>
2019-06-19 16:40:19 +0300

    Testing

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -5,7 +5,7 @@ import pipeline.Deployer
 def call(body) {
     this.echo "Build Docker image stage..."
     this.stage("Build Docker image") {
-        new Sample(1, 2)
+        new Deployer(script:this).run()
     }
     return this
 }
