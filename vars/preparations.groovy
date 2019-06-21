6101c572f01843d33aafc53c1371f0cf40932c52
Husan Kateryna <dgivasss@gmail.com>
2019-06-21 12:24:51 +0300

    Testing

--- vars/preparations.groovy
+++ vars/preparations.groovy
@@ -6,10 +6,10 @@ def call(body) {
     this.echo "Preparations stage..."
     this.stage("Preparations") {
         this.echo  "${params}"
-        GitServiceImpl gitService = new GitServiceImpl(this, "https://github.com/vpogorelovmapr/jenkins-pipeline-shared-lib-sample",
+        GitServiceImpl gitService = new GitServiceImpl(this, "https://github.com/vpogorelovmapr/websocket-lobby-service",
         "33f26755-b867-45ac-b647-3e22e1903437", "vpogorelovmapr")
         gitService.cleanUp()
-        gitService.checkout("master")
+        gitService.checkout("develop")
     }
     return this
 }
